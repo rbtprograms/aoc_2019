0bfd756517e062198ef6b97f09084ff389f8fdfe
Robert Thompson <rbtprograms@gmail.com>
2019-12-05 19:20:27 -0800

    refactor

--- 3/pt1.py
+++ 3/pt1.py
@@ -1,33 +1,28 @@
 def get_manhattan_distance(coords):
-  if coords == 0:
-    coords = [0,0]
   return abs(0 - coords[0]) + abs(0 - coords[1])
 
 def get_coords(instructions):
-  coord = (0, 0)
-  coords = set(coord)
+  x, y = 0, 0
+  coords = set()
 
   for instruction in instructions:
     direction = instruction[0]
     movement = instruction[1:]
 
-    for x in range(0, int(movement)):
+    for _ in range(0, int(movement)):
       if direction == 'R':
-        coord = (coord[0] + 1, coord[1])
+        x += 1
       if direction == 'L':
-        coord = (coord[0] - 1, coord[1])
+        x -= 1
       if direction == 'U':
-        coord = (coord[0], coord[1] + 1)
+        y += 1
       if direction == 'D':
-        coord = (coord[0], coord[1] - 1)
-      coords.add(coord)
+        y -= 1
+      coords.add((x,y))
   return coords
-
-# get_coords('1,2')
 
 with open('input.txt') as inputfile:
   lines = [get_coords(line.split(',')) for line in inputfile]
   crossings = lines[0].intersection(lines[1])
-  result = [get_manhattan_distance(crossing) for crossing in crossings]
-  result.sort()
+  result = min([get_manhattan_distance(crossing) for crossing in crossings])
   print("RESULT ", result)
