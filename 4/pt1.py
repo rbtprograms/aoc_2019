10aafe62a875a976d19606520873fa702fce89b7
Robert Thompson <rbtprograms@gmail.com>
2019-12-05 21:04:30 -0800

    day4 pt 1 done

---
+++ 4/pt1.py
@@ -0,0 +1,9 @@
+count = 0
+for x in range(273025,767253):
+  digits = str(x)
+  pairs = any([digits[i] == digits[i+1] for i in range(len(digits)-1)])
+  next_is_smaller = any([digits[i] > digits[i+1] for i in range(len(digits)-1)])
+  if pairs and not next_is_smaller:
+    count += 1
+
+print(count)
