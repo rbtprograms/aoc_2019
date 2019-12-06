5f7c049f3831d54fbf355be1d4396efbd7e7f688
Robert Thompson <rbtprograms@gmail.com>
2019-12-06 13:12:42 -0800

    done

--- 4/pt2.py
+++ 4/pt2.py
@@ -1,3 +1,25 @@
+
+# def count_digits(number):
+#   digits_dict = {}
+#   for digit in number:
+#     if digit in digits_dict:
+#       digits_dict[digit] += 1
+#     else:
+#       digits_dict[digit] = 1
+#   if 2 in digits_dict.values():
+#     return True
+#   return False
+
+# for x in range(273025,767253):
+#   digits = str(x)
+#   pairs = any([count_digits(digits) for i in range(len(digits)-1)])
+#   next_is_smaller = any([digits[i] > digits[i+1] for i in range(len(digits)-1)])
+#   if pairs and not next_is_smaller:
+#     count += 1
+
+# print(count)
+
+# this literally felt so simple that I implemented something similar above because it felt too easy
 count = 0
 
 for x in range(273025,767253):
